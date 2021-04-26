96e078ec436d2c406a34cb1bf5b8102619601845
Bluelego24 <79273557+Bluelego24@users.noreply.github.com>
2021-04-26 10:02:40 -0600

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,99 @@
+ 
+
+
+> Open this page at [https://nekolisha.github.io/platformer/](https://nekolisha.github.io/platformer/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/nekolisha/platformer** and import
+
+## Edit this project ![Build status badge](https://github.com/nekolisha/platformer/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/nekolisha/platformer** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/nekolisha/platformer/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
+
+
+
+> Open this page at [https://nekolisha.github.io/main-platrmer/](https://nekolisha.github.io/main-platrmer/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/nekolisha/main-platrmer** and import
+
+## Edit this project ![Build status badge](https://github.com/nekolisha/main-platrmer/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/nekolisha/main-platrmer** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/nekolisha/main-platrmer/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
+
+
+
+> Open this page at [https://bluelego24.github.io/err-404-restart-initiated/](https://bluelego24.github.io/err-404-restart-initiated/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/bluelego24/err-404-restart-initiated** and import
+
+## Edit this project ![Build status badge](https://github.com/bluelego24/err-404-restart-initiated/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/bluelego24/err-404-restart-initiated** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/bluelego24/err-404-restart-initiated/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,175 @@
+namespace SpriteKind {
+    export const NPC = SpriteKind.create()
+    export const NPC2 = SpriteKind.create()
+    export const NPC3 = SpriteKind.create()
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
+    if (Level == 1) {
+        game.showLongText("\"Congratulations on beating level 1!\" *You swear you heard a crash in your yard* \"Press A to Start Level 2!", DialogLayout.Full)
+        game.splash("ERR404")
+        game.splash("Restart Initiated:", "Please Retry")
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(1, 14))
+        Level = 2
+    } else if (Level == 2) {
+        game.showLongText("\"Congratulations on beating level 1!\" *You think you hear a window downstairs break* \"Press A to Start Level 2!", DialogLayout.Full)
+        game.splash("ERR404")
+        game.splash("Rest@rt 1niti@ted:", "Ple@se Retry")
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(1, 14))
+        Level = 3
+    } else if (Level == 3) {
+        Level = 4
+        game.showLongText("\"Congratulations on beating level 1!\" *You hear the door to your room creak open* \"Press A to Start Level 2!", DialogLayout.Full)
+        game.splash("ERR404")
+        game.splash("R3st@rt 1niti@t3d:", "Pl3@s3 R3try")
+        NPC2.setImage(assets.image`myImage2`)
+        animation.stopAnimation(animation.AnimationTypes.All, NPC2)
+    } else if (Level == 4) {
+        NPC1.setImage(assets.image`myImage`)
+        animation.stopAnimation(animation.AnimationTypes.All, NPC1)
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(1, 14))
+    } else {
+        game.splash("ERR666")
+        game.splash("Failure:", "Game Executed")
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.NPC, function (sprite, otherSprite) {
+    if (Level == 1 && TT1 == 0) {
+        game.showLongText("*Happily Blurbs* blurbblurb", DialogLayout.Bottom)
+        TT1 = 1
+    } else if (Level == 2 && TT1 == 1) {
+        game.showLongText("bLuRbblurBBlurb", DialogLayout.Bottom)
+        TT1 = 2
+    } else if (Level == 3 && TT1 == 2) {
+        game.showLongText("BLURBBLURBBLURBBLURB", DialogLayout.Bottom)
+        TT1 = 3
+    } else if (Level == 4 && TT1 == 3) {
+        game.showLongText("...", DialogLayout.Bottom)
+        TT1 = 4
+    } else {
+    	
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`tile14`, function (sprite, location) {
+    if (controller.A.isPressed()) {
+        mySprite.vy = -150
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.NPC2, function (sprite, otherSprite) {
+    if (Level == 1 && TT2 == 0) {
+        game.showLongText("*Slime bounces to a stop* Hello there!!!", DialogLayout.Bottom)
+        TT2 = 1
+    } else if (Level == 2 && TT2 == 1) {
+        game.showLongText("*Slime slams into ground hard* HELLO there!!!", DialogLayout.Bottom)
+        TT2 = 2
+    } else if (Level == 3 && TT2 == 2) {
+        game.showLongText("*Slime hit the ground too hard *", DialogLayout.Bottom)
+        TT2 = 3
+    } else if (Level == 4 && TT2 == 3) {
+        game.showLongText("...", DialogLayout.Bottom)
+        TT2 = 4
+    } else {
+    	
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`tile3`, function (sprite, location) {
+    mySprite.destroy()
+    pause(100)
+    Respawn()
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.NPC3, function (sprite, otherSprite) {
+    let TT3 = 0
+    if (Level == 1 && TT3 == 0) {
+        game.showLongText("Your New", DialogLayout.Bottom)
+        TT2 = 1
+    } else if (Level == 2 && TT3 == 1) {
+        game.showLongText("Your still here", DialogLayout.Bottom)
+        TT2 = 2
+    } else if (Level == 3 && TT3 == 2) {
+        game.showLongText("*whispers to self* They usually don't last this long...", DialogLayout.Bottom)
+        TT2 = 3
+    } else if (Level == 4 && TT3 == 3) {
+        game.showLongText("Chuckles softly", DialogLayout.Bottom)
+        TT2 = 4
+    } else {
+    	
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`tile11`, function (sprite, location) {
+    if (controller.A.isPressed()) {
+        mySprite.vy = -150
+    }
+})
+function Respawn () {
+    mySprite = sprites.create(assets.image`myImage0`, SpriteKind.Player)
+    scene.cameraFollowSprite(mySprite)
+    controller.moveSprite(mySprite, 100, 0)
+    mySprite.ay = 480
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(1, 14))
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`tile5`, function (sprite, location) {
+    if (controller.A.isPressed()) {
+        mySprite.vy = -150
+    }
+})
+function GameStart () {
+    scene.setBackgroundColor(8)
+    tiles.setTilemap(tilemap`level1`)
+    mySprite = sprites.create(assets.image`myImage0`, SpriteKind.Player)
+    NPC1 = sprites.create(assets.image`slime1`, SpriteKind.NPC)
+    NPC2 = sprites.create(assets.image`slime`, SpriteKind.NPC2)
+    NPC3 = sprites.create(assets.image`myImage3`, SpriteKind.NPC3)
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(1, 14))
+    scene.cameraFollowSprite(mySprite)
+    controller.moveSprite(mySprite, 100, 0)
+    mySprite.ay = 480
+    tiles.placeOnRandomTile(NPC1, assets.tile`myTile1`)
+    tiles.placeOnRandomTile(NPC2, assets.tile`Temporary asset`)
+    Level = 1
+    Music = 1
+    animation.runImageAnimation(
+    NPC1,
+    assets.animation`clownFishLeft`,
+    100,
+    true
+    )
+    animation.runImageAnimation(
+    NPC2,
+    assets.animation`myAnim`,
+    200,
+    true
+    )
+    animation.runImageAnimation(
+    NPC3,
+    assets.animation`myAnim0`,
+    950,
+    true
+    )
+}
+let Music = 0
+let NPC3: Sprite = null
+let TT2 = 0
+let TT1 = 0
+let NPC1: Sprite = null
+let NPC2: Sprite = null
+let mySprite: Sprite = null
+let Level = 0
+scene.setBackgroundColor(15)
+game.splash("ERR 404:", "RESTART INITIATED")
+game.splash("A Horror Platformer by:", "Team Huuuuuuuuumins")
+game.splash("Press A to begin", "your game")
+scene.setBackgroundColor(10)
+game.showLongText("*You bought a retro platformer called Jumpy Block*", DialogLayout.Center)
+game.showLongText("It had no reviews or ratings but it appeared that 341 people had bought it, probabaly some kind of glitch.", DialogLayout.Center)
+game.showLongText("*You turn on your new game* \"Welcome to Jumpy Block! Press A to begin your jumping experience!\"", DialogLayout.Center)
+GameStart()
+forever(function () {
+    if (Music == 1) {
+        music.playMelody("C5 A G F A G F D ", 120)
+    } else if (Music == 2) {
+        music.playMelody("E B C5 A B G A F ", 120)
+    } else if (Music == 3) {
+        music.playMelody("C G A F G E F D ", 40)
+    } else {
+    	
+    }
+})
